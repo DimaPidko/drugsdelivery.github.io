51c630f9d9ffff56a7240adae2f7dc32374451ab
Dmitriy Pidko <pidkodima@gmail.com>
2024-03-11 09:20:44 +0200

    styles +++

--- src/components/ui/shop/navigation/logInForm/LogInForm.jsx
+++ src/components/ui/shop/navigation/logInForm/LogInForm.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/prop-types */
+import styles from './LogInForm.module.sass';
+
 const LogInForm = (props) => {
     return (
         <>
@@ -29,16 +31,24 @@ const LogInForm = (props) => {
             ) : (
                 <div>
                     <h3>Log In</h3>
-                    <form onSubmit={props.onLogIn}>
+                    <form
+                        onSubmit={props.onLogIn}
+                        className={styles.formIn}>
+                        <label htmlFor="login">Login</label>
                         <input
                             type="text"
                             value={props.login}
                             onChange={(e) => props.setLogin(e.target.value)}
+                            className={styles.formIn__inpt}
+                            id="login"
                         />
+                        <label htmlFor="password">Password</label>
                         <input
                             type="password"
                             value={props.password}
                             onChange={(e) => props.setPassword(e.target.value)}
+                            className={styles.formIn__inpt}
+                            id="password"
                         />
                         <button>Login</button>
                     </form>
